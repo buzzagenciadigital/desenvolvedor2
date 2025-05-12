34ccce0070129a46acdbcfd6d13046cf00f1b9d8
Celes Jr <30604277+buzzagenciadigital@users.noreply.github.com>
2025-05-12 08:47:26 -0300

    Create 	05_inverter_string.py

---
+++ 05_inverter_string.py
@@ -0,0 +1,9 @@
+def inverter_string(s):
+    invertida = ''
+    for caractere in s:
+        invertida = caractere + invertida
+    return invertida
+
+# Exemplo de uso:
+texto = "Guilherme"
+print(inverter_string(texto))  # emrehliuG
